5d14a82c4918ea7b4ee6606bfa7b0bb8cc442943
Sahil Shrestha <146646189+sahilstha0007@users.noreply.github.com>
2025-02-05 18:16:52 +0545

    this

--- Prj/paste-app/src/components/Home.jsx
+++ Prj/paste-app/src/components/Home.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useSearchParams } from 'react-router-dom';
 
-const Home = () => {
+function Home() {
+    const [title, setTitle] = useState('')
+    const [value, setValue] = useState('');
+    const [useSearchParams, setUseSearchParams] = useSearchParams()
+    const pasteId = setUseSearchParams.get("pasteId")
+
+
     return (
         <div>
-            Home
+            <input
+            className="  border border-amber-700 rounded-2xl  py-2 mt-2 "
+            type="text" placeholder='Enter title here' 
+            value={title} onClick={(e) => setTitle(e.target.value)} />
+
+            <button className=' bg-amber-700 py-2 px-5 mx-5 rounded-md' >Create My </button>
         </div>
     )
 }
